aaf84e8825b5b93a592890584acbfd4b499d0aa6
Tom Daley <tjd@powerdaley.com>
2023-07-29 16:40:54 -0500

    realistic data look good

--- app.py
+++ app.py
@@ -4,30 +4,78 @@ app.py is the main file for the application. It contains the main function.
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import json
 import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Sample school calendar data
+# Sample school calendar data - This will be a single document in the database.
 school_calendar_data = {
 	"district_id": "Dallas ISD",
-	"years": [
-		{
-			"2022-2023": [
-				{"01/01/2022": {"description": "New Year's Holiday", "category": "Holiday"}},
-				{"01/14/2022": {"description": "President's Day", "category": "Holiday"}},
-				{"01/15/2022": {"description": "Grading Period", "category": "GradingPeriodStartStop"}},
-			]
+	"years": {
+		"2022-2023": {
+			"08/09/2022": {"description": "First Day of School", "category": "FirstDayofSchool"},
+			"01/01/2023": {"description": "New Year's Holiday", "category": "Holiday"},
+			"01/14/2023": {"description": "President's Day", "category": "Holiday"},
+			"01/15/2023": {"description": "Grading Period", "category": "GradingPeriodStartStop"},
+			"05/24/2023": {"description": "Last Day of School", "category": "LastDayofSchool"}
 		},
-		{
-			"2023-2024": [
-				{"01/01/2024": {"description": "New Year's Holiday", "category": "Holiday"}},
-				{"01/15/2024": {"description": "President's Day", "category": "Holiday"}},
-				{"01/16/2022": {"description": "Grading Period", "category": "GradingPeriodStartStop"}},
-			]
-		},
-	],
+		"2023-2024": {
+			"08/14/2023": {"description": "First Day of School", "category": "FirstDayofSchool"},
+
+			"08/03/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"08/07/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"08/08/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"08/09/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"10/16/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"11/07/2023": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"01/08/2024": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+			"02/19/2024": {"description": "Professional Development (No school)", "category": "staffdevelopment"},
+
+			"08/04/2023": {"description": "Teacher Work Day (No school)", "category": "teacherworkday"},
+			"08/10/2023": {"description": "Teacher Work Day (No school)", "category": "teacherworkday"},
+			"08/11/2023": {"description": "Teacher Work Day (No school)", "category": "teacherworkday"},
+
+			"09/04/2023": {"description": "Labor Day", "category": "Holiday"},
+			"11/20/2023": {"description": "Thanksgiving Break", "category": "Holiday"},
+			"11/21/2023": {"description": "Thanksgiving Break", "category": "Holiday"},
+			"11/22/2023": {"description": "Thanksgiving Break", "category": "Holiday"},
+			"11/23/2023": {"description": "Thanksgiving Break", "category": "Holiday"},
+			"11/24/2023": {"description": "Thanksgiving Break", "category": "Holiday"},
+			"12/25/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/26/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/27/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/28/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/29/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/30/2023": {"description": "Winter Break", "category": "Holiday"},
+			"12/31/2023": {"description": "Winter Break", "category": "Holiday"},
+			"01/01/2024": {"description": "Winter Break", "category": "Holiday"},
+			"01/02/2024": {"description": "Winter Break", "category": "Holiday"},
+			"01/03/2024": {"description": "Winter Break", "category": "Holiday"},
+			"01/04/2024": {"description": "Winter Break", "category": "Holiday"},
+			"01/05/2024": {"description": "Winter Break", "category": "Holiday"},
+			"03/11/2024": {"description": "Spring Break", "category": "Holiday"},
+			"03/12/2024": {"description": "Spring Break", "category": "Holiday"},
+			"03/13/2024": {"description": "Spring Break", "category": "Holiday"},
+			"03/14/2024": {"description": "Spring Break", "category": "Holiday"},
+			"03/15/2024": {"description": "Spring Break", "category": "Holiday"},
+			"03/29/2024": {"description": "Holiday", "category": "Holiday"},
+			"05/27/2024": {"description": "Memorial Day", "category": "Holiday"},
+			"06/19/2024": {"description": "Juneteenth", "category": "Holiday"},
+			"10/12/2023": {"description": "Fall Break", "category": "Holiday"},
+			"10/13/2023": {"description": "Fall Break", "category": "Holiday"},
+
+			"12/22/2023": {"description": "Early Release", "category": "EarlyRelease"},
+
+			"04/01/2024": {"description": "Inclement Weather Day", "category": "BadWeatherDay"},
+			"05/03/2024": {"description": "Inclement Weather Day", "category": "BadWeatherDay"},
+
+			"01/16/2022": {"description": "Grading Period", "category": "GradingPeriodStartStop"},
+
+			"05/24/2024": {"description": "Last Day of School", "category": "LastDayofSchool"}
+		}
+	}
 }
 
 
@@ -36,19 +84,27 @@ def get_school_calendar(district_id: str, school_year: str):
 	# Assuming school_year will be in the format "YYYY-YYYY" (e.g., "2022-2023")
 	print("district_id: ", district_id)
 	print("school_year: ", school_year)
-	for year_data in school_calendar_data["years"]:
-		if school_year in year_data:
-			return year_data[school_year]
-
-	return {"error": "School year not found"}
-
+	if not school_calendar_data:
+		return {"error": f"District not found: {district_id}"}
+	
+	years = school_calendar_data.get("years")
+	if not years:
+		return {"error": "School years data not found - database error"}
+	
+	events = years.get(school_year)
+	if not events:
+		return {"error": f"School year not found. School year: {school_year} District: {district_id}"}
+	
+	print(json.dumps(events, indent=4))
+	
+	return events
 
 @app.get("/calendar")
 async def show_calendar(request: Request):
 	template = "calendar.html"
 
 	# You can pass any context variables required for rendering dynamic content
-	context = {"title": "School Calendar - Dallas ISD", "district_id": "Dallas ISD"}
+	context = {"title": "School Calendar - Dallas ISD", "district_id": "Dallas ISD", "school_year": "2022-2023"}
 
 	# Render the HTML page with the template and context
 	return templates.TemplateResponse(template, {"request": request, **context})
--- static/calendar.js
+++ static/calendar.js
@@ -1,89 +1,183 @@
-function fetchData() {
-	  //const endpoint = "https://schoolcalendar.jdbot.us/isd/Dallas%20ISD/year/2022-2023";
-	  const endpoint = "http://localhost:8000/isd/Dallas%20ISD/year/2022-2023";
+function fetchData(school_name, year_range) {
+	//const endpoint = "https://schoolcalendar.jdbot.us/isd/Dallas%20ISD/year/2022-2023";
+	const endpoint = `http://localhost:8000/isd/${school_name}/year/${year_range}`;
+	console.log(endpoint);
 
-	  // Make a GET request to the API endpoint
-	  fetch(endpoint)
+	// Make a GET request to the API endpoint
+	fetch(endpoint)
 		.then(response => response.json())
 		.then(data => {
-		  // Process the data and generate the calendar
-		  	displayCalendar(data);
+			// Process the data and generate the calendar
+			displayCalendar(data, year_range);
 		})
-		  .catch(error => {
-		  	console.error('Error fetching data:', error);
+		.catch(error => {
+			console.error('Error fetching data:', error);
 		});
+}
+
+class Months {
+	constructor(startYear, endYear) {
+		this.months = [
+			"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+		];
+
+		/* These are month indices, not month numbers. */
+		this.monthSequence = [7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6]; // Start with August
+		this.startYear = startYear;
+		this.endYear = endYear;
 	}
 
-	// Function to display the calendar with events
-function displayCalendar(yearData) {
+	getMonthName(month_idx) {
+		return this.months[month_idx];
+	}
+
+	/**
+	 * 
+	 * @param {*} month index of the month, not the month number, so Jan = 0, Feb = 1, etc.
+	 * @returns 
+	 */
+	getApplicableYear(month) {
+		if (month < this.monthSequence[0]) {
+			return this.endYear;
+		} else {
+			return this.startYear;
+		}
+	}
+
+	/**
+	 * 
+	 * @param {*} month index of the month, not the month number, so Jan = 0, Feb = 1, etc.
+	 * @returns
+	 * 		Number of days in the month
+	 */
+	getMaxMonthDays(month) {
+		const year = this.getApplicableYear(month);
+		const date = new Date(year, month, 0);
+		return date.getDate();
+	}
+}
+
+// Function to display the calendar with events
+function displayCalendar(yearData, year_range) {
 	console.log(yearData);
-	  const calendarDiv = document.getElementById("calendar");
+	const years = year_range.split('-');
+	const startYear = years[0];
+	const endYear = years[1];
+	const MonthsObj = new Months(startYear, endYear);
+	const calendarDiv = document.getElementById("calendar");
 
-	  // Create a table to represent the calendar
-	  const table = document.createElement("table");
+	// Create a table to represent the calendar
+	const table = document.createElement("table");
 
-	  // Add header row
-	  const headerRow = document.createElement("tr");
-	  const months = [
-		"January", "February", "March", "April", "May", "June",
-		"July", "August", "September", "October", "November", "December"
-	  ];
+	// Add header row
+	const headerRow = document.createElement("tr");
 
-	  months.forEach(month => {
+	MonthsObj.monthSequence.forEach(month => {
 		const th = document.createElement("th");
-		th.textContent = month;
+		th.textContent = MonthsObj.getMonthName(month);
 		headerRow.appendChild(th);
-	  });
+	});
 
-	  table.appendChild(headerRow);
+	table.appendChild(headerRow);
 
-	  // Add rows for each day in the months
-	  if (yearData) {
+	// Add rows for each day in the months
+	if (yearData) {
 
 		for (let i = 1; i <= 31; i++) { // Assuming each month has 31 days
-		  const row = document.createElement("tr");
+			const row = document.createElement("tr");
 
-		  months.forEach(month => {
-			  const td = document.createElement("td");
-			  //td.classList.add('event-tooltip-container');
-			  //td.classList.add('event-tooltip-trigger');
+			/* Looping thru each month index, not month number. Jan = 0, Feb = 1, etc. */
+			MonthsObj.monthSequence.forEach(month => {
+				if (i > MonthsObj.getMaxMonthDays(month+1)) {
+					// Skip this day if it doesn't exist in the current month by appending an empty cell.
+					const td = document.createElement("td");
+					row.appendChild(td);
+				} else {
+					const td = document.createElement("td");
+					td.classList.add('event-tooltip-container');
 
-			// Get the events for the current month and day
-			  const currentDay = `${i}`.padStart(2, "0"); // Pad the day with a leading 0 if needed
-			  const currentMonth = `${months.indexOf(month) + 1}`.padStart(2, "0"); // Pad the month with a leading 0 if needed
-			  const currentDate = `${currentMonth}/${currentDay}/2022`;
-			  td.appendChild(document.createTextNode(currentDay));
-			const currentEvents = yearData.filter(event => event[currentDate]);
+					// Get the events for the current month and day
+					const currentDay = `${i}`.padStart(2, "0"); // Pad the day with a leading 0 if needed
+					const currentMonth = `${month + 1}`.padStart(2, "0"); // Pad the month with a leading 0 if needed
+					const currentYear = MonthsObj.getApplicableYear(month);
+					const currentDate = `${currentMonth}/${currentDay}/${currentYear}`;
+					const dateNode = document.createTextNode(currentDay);
+					td.appendChild(dateNode);
+					myEvent = yearData[currentDate];
 
-			// Add the events to the cell
-			  currentEvents.forEach(event => {
-				  console.log(event[currentDate]);
-				const eventDescription = event[currentDate].description;
-				  const eventCategory = event[currentDate].category.toLowerCase();
-				  const className = 'event-type-' + eventCategory;
-				console.log(eventDescription + '  ' + eventCategory);
-			  const eventElement = document.createElement('p');
-				  eventElement.textContent = eventDescription;
-				 //eventElement.classList.add(className);
-				  td.classList.add(className);
-			  td.appendChild(eventElement);
-			//	  const tooltip = document.createElement('div');
-			//	  tooltip.classList.add('event-tooltip');
-			//	  tooltip.textContent = eventDescription;
-			//	  td.appendChild(tooltip);
+					// The array will be necessary once we start showing more than one school district at a time.
+					const currentEvents = myEvent ? [myEvent] : [];
+
+					// Add the events to the cell
+					currentEvents.forEach(event => {
+						const eventDescription = event.description;
+						const eventCategory = event.category.toLowerCase();
+						const className = 'event-type-' + eventCategory;
+						td.classList.add(className);
+						addTooltip(td, eventDescription);
+					});
+
+					if (currentEvents.length == 0) {
+						const myDate = new Date(currentYear, month, currentDay);
+						const dayOfWeek = myDate.getDay();
+						if (dayOfWeek == 0 || dayOfWeek == 6) {
+							var className = 'event-type-weekend';
+							if (isFirstThirdFifthWeekend(myDate)) {
+								className = 'event-type-135weekend';
+							}
+							td.classList.add(className);
+							addTooltip(td, 'Weekend');
+						}
+					}
+
+					row.appendChild(td);
+				}
 			});
 
-			row.appendChild(td);
-		  });
-
-		  table.appendChild(row);
+			table.appendChild(row);
 		}
-	  }
-
-	  calendarDiv.appendChild(table);
 	}
 
-	// Fetch data and display the calendar when the page loads
-	window.onload = function() {
-	  fetchData();
-	};
+	calendarDiv.appendChild(table);
+}
+
+function addTooltip(parentElement, tooltipText) {
+	const tooltip = document.createElement('div');
+	tooltip.classList.add('event-tooltip');
+	tooltip.textContent = tooltipText;
+	parentElement.appendChild(tooltip);
+}
+
+function previousDate(date, daysback=1) {
+	const onDayInMillis = 24 * 60 * 60 * 1000 * daysback;
+	const previousDayInMillis = date.getTime() - onDayInMillis;
+	const previousDay = new Date(previousDayInMillis);
+	return previousDay;
+}
+
+/**
+ * 
+ * @param {*} date  A date object (must be a saturday or sunday)
+ * @returns boolean true if the date is the first, third, or fifth weekend of the month
+ */
+function isFirstThirdFifthWeekend(date) {
+	const dayOfWeek = date.getDay();
+	const lookback = dayOfWeek == 6 ? 1 : 2;
+	const previousFriday = previousDate(date, lookback);
+	if (previousFriday.getDay() != 5) {
+		console.log('Error: previous day is not a friday');
+		return false;
+	}
+	const dayOfMonth = previousFriday.getDate();
+	const isFirstFriday = dayOfMonth <= 7;
+	const isThirdFriday = dayOfMonth >= 15 && dayOfMonth <= 21;
+	const isFifthFriday = dayOfMonth >= 29;
+	return isFirstFriday || isThirdFriday || isFifthFriday;
+}
+
+
+// Fetch data and display the calendar when the page loads
+window.onload = function () {
+	fetchData("Dallas ISD", "2023-2024");
+};
